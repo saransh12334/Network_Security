3d7fe9036993246a2c54a3c99beae9ceffba4dce
saransh12334 <saransh12334@users.noreply.github.com>
2018-03-18 12:22:51 +0530

    Update Captive_Portal.sh

--- Captive_Portal.sh
+++ Captive_Portal.sh
@@ -3,16 +3,16 @@
 echo "HEY THERE"
 
 
-echo "WELCOME TO CAPTIVE_PORTAL_SCRIPT!"
-echo  "LET'S PROCEED"
-echo  " 1. Lets Download the required softwares for the operation first "
+echo "[*]WELCOME TO CAPTIVE_PORTAL_SCRIPT!"
+echo  "[*]LET'S PROCEED"
+echo  "[*]Lets Download the required softwares for the operation first "
 
 	apt-get update
 	apt-get install dnsmasq
 	apt-get install hostapd
 
-echo " 2. Now the softwares have been downloaded "
-echo " Clearing the iptables "
+echo "[*]Now the softwares have been downloaded "
+echo "[*]Clearing the iptables "
 
 	echo 1 > /proc/sys/net/ipv4/ip_forward
 	iptables --flush
@@ -21,19 +21,19 @@ echo " Clearing the iptables "
 	iptables --table nat --delete-chain
 
 
-echo " 3. Stopping the network-manager "
+echo "[*]Stopping the network-manager "
 
 	service network-manager stop
 
-echo " 4. Now Moving Forward "
+echo "[*]Now Moving Forward "
 	
-echo " Enabling DHCP server and DNS server "
+echo "[*]Enabling DHCP server and DNS server "
 
 	dnsmasq -C /root/Downloads/fake-ap/dnsmasq.conf
 
-echo "  DONE "
+echo "[*]DONE "
 
-echo " Configuring wlan0 to the ip addresses"
+echo "[*]Configuring wlan0 to the ip addresses"
 
 	ifconfig wlan0 up
         ifconfig wlan0	
@@ -43,27 +43,27 @@ echo " Configuring wlan0 to the ip addresses"
 	ifconfig wlan0 up
 	ifconfig wlan0
 
-echo " 5. Enabling hostapd server "
+echo "[*]Enabling hostapd server "
 
 	hostapd /root/Downloads/fake-ap/hostapd.conf -B
 
-echo " 6. and now starting apache2 web server "
+echo "[*]and now starting apache2 web server "
 
 	service apache2 start 
 
-echo " 7. Now wait for a while let the clients connect "
+echo "[*]Now wait for a while let the clients connect "
 
-echo " Meanwhile"
+echo "[*]Meanwhile"
 
-echo " Opening Wireshark "
+echo "[*]Opening Wireshark "
 
-echo "Please Look for HTTP request and POST methods "
+echo "[*]Please Look for HTTP request and POST methods "
 
 	wireshark
 
-echo " Thank you "
+echo "[*]Thank you "
 
-echo " Good day "
+echo "[*]Good day "
 
 echo " Stopping all the services for you "
 
